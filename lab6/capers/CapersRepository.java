b91cac77e2ce53f58677c90e352f26c6930c21dd
Kettenkrado <miaolingtong@yeah.net>
2024-05-27 17:41:17 +0800

    correct lab6: the directory position

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -19,7 +19,7 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = join(CWD, "capers", ".capers");
+    static final File CAPERS_FOLDER = join(CWD,  ".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -47,7 +47,7 @@ public class CapersRepository {
         File story = new File(CAPERS_FOLDER, "story.txt");
         if (!story.exists()){
             story.createNewFile();
-            writeContents(story,  text, "\n");
+            writeContents(story, text, "\n");
         } else {
             String originalText = readContentsAsString(story);
             writeContents(story, originalText, text, "\n");
